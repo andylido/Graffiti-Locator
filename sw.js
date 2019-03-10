6a9d64e820f01f41cbe457f66b468a6fad92ca22
[Andy] <ali37@uic.edu>
2019-03-10 00:04:13 -0600

    Service worker added

--- sw.js
+++ sw.js
@@ -1,40 +1,19 @@
-/*
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
 
+version = '1.0';
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-});
+let cacheName = 'pwaBasic_' + version;
 
-var CACHE_NAME = 'GRemoval';
-var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', e => {
+  let timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `./`,
+        `./index.html`
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
 });
 
 // https://stackoverflow.com/questions/41009167/what-is-the-use-of-self-clients-claim
